c65de771c1ab24ffcbe7ace66a21d0ebae4a4af3
Bodochka <bobrby2010@gmail.com>
2025-03-01 02:15:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,4 @@
-import os
-from dotenv import load_dotenv
-
-# Загрузка переменных из .env
-load_dotenv()
-
 # Конфигурация
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-CRYPTO_PAY_TOKEN = os.getenv("CRYPTO_PAY_TOKEN")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
+BOT_TOKEN = "токен вашего бота"
+CRYPTO_PAY_TOKEN = "ваш апи токен из @send"
+DATABASE_NAME = "shop_bot.db"
